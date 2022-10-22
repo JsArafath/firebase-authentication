e20cddcb2f0a7b016fc863f30a598b798066fd3e
JsArafath <arafatrahman077777@gmail.com>
2022-10-22 18:44:47 +0600

    react

--- src/App.js
+++ src/App.js
@@ -1,54 +1,13 @@
 import './App.css';
-import {getAuth, signInWithPopup, GoogleAuthProvider, signOut, GithubAuthProvider} from 'firebase/auth';
-import app from './Firebase/Firebase.init';
-import { useState } from 'react';
 import ReactBootstrap from './Component/ReactBootstrap';
 
-const auth = getAuth(app);
+
 
 function App() {
 
 
 
-  const [user, setUser] = useState ({})
-  const GoogleProvider = new GoogleAuthProvider();
-  const GithubProvider = new GithubAuthProvider();
-
-
-  const handleGoogleSignIn = () => {
-  signInWithPopup (auth, GoogleProvider)
-  .then(result => {
-    const user = result.user;
-    setUser(user);
-    console.log(user);
-  })
-  .catch(error => {
-    console.log('error:', error);
-  })
- }
-
- const handleGoogleSignOut = () => {
-  signOut(auth)
-  .then(() => {
-    setUser({});
-  })
-  .catch(() => {
-    setUser({});
-  })
- }
-
- const handleGithubSignIn = () => {
-
-  signInWithPopup(auth, GithubProvider)
-  .then(result => {
-    const user = result.user;
-    setUser(user);
-    console.log(user);
-  })
-  .catch(error => {
-    console.log('error:', error);
-  })
- }
+  
 
     return (
       
@@ -57,22 +16,7 @@ function App() {
         <ReactBootstrap></ReactBootstrap>
         </div>
 
-        { user.uid ?
-        <button className='borderr' onClick={handleGoogleSignOut}> Google Sign Out </button>
-          :
-        <div>
-          <button className='borderr' onClick={handleGoogleSignIn}> Google Sign In </button>
-          <button className='borderr' onClick={handleGithubSignIn}> Github Sign In </button>
-        </div>
-        }
-
-        { user.uid &&
-          <div>
-            <h3> Name: {user.displayName}</h3>
-            <p> Email: {user.email} </p>
-            <img className='photo-1' src={user.photoURL} alt="" />
-          </div>
-        }
+       
         
       </div>
     );
--- src/Component/ReactBootstrap.js
+++ src/Component/ReactBootstrap.js
@@ -1,6 +1,13 @@
 import React from 'react';
+
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, signInWithPopup, GoogleAuthProvider, signOut, GithubAuthProvider } from 'firebase/auth';
+
+import app from './../Firebase/Firebase.init';
+
+const auth = getAuth (app);
 
 const ReactBootstrap = () => {
 
@@ -9,7 +16,77 @@ const ReactBootstrap = () => {
         const email = event.target.email.value;
         const password = event.target.password.value;
         console.log(email, password);
+        createUserWithEmailAndPassword(auth, email, password)
+
+        .then((result) => {
+            const user = result.user;
+            console.log(user);
+          })
+          .catch((error) => {
+           console.log('error:' , error);
+          });
     }
+
+
+
+
+
+
+
+
+
+    const [user, setUser] = useState ({})
+    const GoogleProvider = new GoogleAuthProvider();
+    const GithubProvider = new GithubAuthProvider();
+  
+  
+    const handleGoogleSignIn = () => {
+    signInWithPopup (auth, GoogleProvider)
+    .then(result => {
+      const user = result.user;
+      setUser(user);
+      console.log(user);
+    })
+    .catch(error => {
+      console.log('error:', error);
+    })
+   }
+  
+   const handleGoogleSignOut = () => {
+    signOut(auth)
+    .then(() => {
+      setUser({});
+    })
+    .catch(() => {
+      setUser({});
+    })
+   }
+  
+   const handleGithubSignIn = () => {
+  
+    signInWithPopup(auth, GithubProvider)
+    .then(result => {
+      const user = result.user;
+      setUser(user);
+      console.log(user);
+    })
+    .catch(error => {
+      console.log('error:', error);
+    })
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
     return (
         <div>  
             <h1 className='text-primary'> Register Here!! </h1> 
@@ -30,6 +107,43 @@ const ReactBootstrap = () => {
             Submit
         </Button>
     </Form>
+
+
+
+
+
+
+
+
+
+    <div className="">
+    
+
+        { user.uid ?
+        <button className='borderr' onClick={handleGoogleSignOut}> Google Sign Out </button>
+          :
+        <div>
+          <button className='borderr' onClick={handleGoogleSignIn}> Google Sign In </button>
+          <button className='borderr' onClick={handleGithubSignIn}> Github Sign In </button>
+        </div>
+        }
+
+        { user.uid &&
+          <div>
+            <h3> Name: {user.displayName}</h3>
+            <p> Email: {user.email} </p>
+            <img className='photo-1' src={user.photoURL} alt="" />
+          </div>
+        }
+        
+      </div>
+
+
+
+
+
+
+
         </div>
     );
 };
